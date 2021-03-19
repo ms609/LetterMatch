676a9c649183c87ad5d09bcafafcca70bc1b918f
ms609 <martins@gmail.com>
2021-03-19 08:03:50 +0000

    v1

--- match.js
+++ match.js
@@ -1,1 +1,32 @@
+RandomLetter = function (not) {
+  var ret = String.fromCharCode(65+Math.floor(Math.random() * 26));
+  if (ret == not) ret = RandomLetter(not);
+  return (ret);
+}
 
+NewLetter = function (avoid) {
+  target = RandomLetter(avoid);
+  $('#target').html(target + target.toLowerCase());
+}
+MarkCorrect = function() {
+  $('#target').addClass("correct");
+  setTimeout(function() {$('#target').removeClass("correct")}, 2000);
+}
+MarkIncorrect = function() {
+  $('#target').addClass("incorrect");
+  setTimeout(function() {$('#target').removeClass("incorrect")}, 800);
+}
+
+var target = ''
+NewLetter();
+
+$(document).keydown(function (e){
+  var pressed = String.fromCharCode(e.keyCode);
+  if (target == pressed) {
+    MarkCorrect();
+    setTimeout(function() {NewLetter(target);}, 2000);
+    target='';
+  } else {
+    if (target != '') MarkIncorrect();
+  }
+})
