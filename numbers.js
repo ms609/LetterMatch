664b4e320ff23038d79427fdbd03dfa363343b79
Martin R. Smith <martin.smith@durham.ac.uk>
2021-07-27 17:14:32 +0100

    # = 0.31

--- numbers.js
+++ numbers.js
@@ -1,6 +1,6 @@
 RandomLetter = function (not) {
   var ret;
-  if (Math.random() < 0.11) {
+  if (Math.random() < 0.31) {
     var ret = String.fromCharCode(48 + Math.floor(Math.random() * 10));    
   } else {
     var ret = String.fromCharCode(65 + Math.floor(Math.random() * 26));
