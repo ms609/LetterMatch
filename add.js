a4387c79ac2f21b14e7868667f4c36a096f44b41
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-05-29 15:08:42 +0100

    Create add.js

---
+++ add.js
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+
+  <title>Letter Match</title>
+  <meta name="description" content="Simple Adding Game" />
+  <meta name="author" content="Martin R. Smith" />
+
+  <link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+  <div id="left"><div id="left-num">??</div><div id="left-block"></div></div>
+  <div id="operator">??</div>
+  <div id="right"><div id="right-num">??</div><div id="right-block"></div></div>
+  <div id="equals">=</div>
+  <div id="answer"><div id="ans-num">??</div><div id="ans-block"></div></div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="common.js"></script>
+  <script src="add.js"></script>
+  <script src="jquery.fireworks.js"></script>
+</body>
+</html>
